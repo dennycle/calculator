ff75203115905c57deb70d810858f03f9aea5f3e
Denny Le and Shawn Lunsford <denny@>
2019-01-14 10:18:17 -0800

    add index.html, scripts.js, directories, and add basic html, and add basic add to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+  return number1 + number2};
+};
+
+var number1 = parseInt(prompt("Pick a number: "));
+var number2 = parseInt(prompt("Pick another number: "));
+var result = add(number1, number2);
+alert(result);
