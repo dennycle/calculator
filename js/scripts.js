4419cc1c6b9e38fef631898f0ce7d97ebf722de8
Denny Le and Shawn Lunsford <shawn@>
2019-01-14 11:15:15 -0800

    add add, subtract, BMI, C>F, and F>C calculations

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,55 @@
-var add = function(number1, number2) {
-  return number1 + number2};
-};
+// ### Addition ###
+// var add = function(number1, number2) {
+//   return number1 + number2;
+// };
+//
+// var number1 = parseInt(prompt("Pick a number: "));
+// var number2 = parseInt(prompt("Pick another number: "));
+// var result = "The sum is " + add(number1, number2) + ".";
+// alert(result);
 
-var number1 = parseInt(prompt("Pick a number: "));
-var number2 = parseInt(prompt("Pick another number: "));
-var result = add(number1, number2);
-alert(result);
+
+// ### Subtraction ###
+// var subtract = function(number1, number2) {
+//   return number1 - number2;
+// };
+//
+// var number1 = parseInt(prompt("Pick a number: "));
+// var number2 = parseInt(prompt("Pick another number: "));
+// var result = "The remainder is " + subtract(number1, number2) + ".";
+// alert(result);
+
+// ### BMI Calculator ###
+// var bmi = function(number1, number2) {
+//   return (number1 / (number2 * number2)) * 703;
+// };
+//
+// var number1 = parseInt(prompt("Enter Weight: "));
+// var number2 = parseInt(prompt("Enter Height in inches: "));
+// var result = bmi(number1, number2);
+// var textresult= "Your BMI is " + result.toFixed() + "!";
+// alert(textresult);
+
+// ### Celsius to Fahrenheit ###
+// var CtoF = function(number1) {
+// 	return (number1 * (9/5) + 32);
+// };
+//
+// var number1 = parseInt(prompt("Enter a temperature in Celsius to be converted to Fahrenheit: "));
+//
+// var result = CtoF(number1);
+// var textresult = "The conversion is " + result.toFixed() + "F.";
+//
+// alert(textresult);
+
+// ### Fahrenheit to Celsius ###
+// var FtoC = function(number1) {
+// 	return ((number1 - 32) * (5/9));
+// };
+//
+// var number1 = parseInt(prompt("Enter a temperature in Fahrenheit to be converted to Celsius: "));
+//
+// var result = FtoC(number1);
+// var textresult = "The conversion is " + result.toFixed() + "C.";
+//
+// alert(textresult);
